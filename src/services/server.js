defd7882e3fc3d3704faed087cf6627b09933d33
Gabriel Rufino <contato@gabrielrufino.com>
2020-12-29 13:53:35 -0300

    Fixing client: app => watcher

--- src/services/server.js
+++ src/services/server.js
@@ -7,7 +7,7 @@ const api = axios.create({
   baseURL: `${SERVER_URL}/api`,
 })
 
-const socket = io(`${SERVER_URL}?client=app`)
+const socket = io(`${SERVER_URL}?client=watcher`)
 
 module.exports = {
   api,
