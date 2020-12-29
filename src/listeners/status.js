dbfe2fb0838947f96fab7b905110ef780b092800
Gabriel Rufino <contato@gabrielrufino.com>
2020-12-29 18:01:26 -0300

    Collecting cpu temperature and memory status

--- src/listeners/status.js
+++ src/listeners/status.js
@@ -3,8 +3,19 @@ const si = require('systeminformation')
 const status = () => {
   const { socket } = require('../services/server')
 
-  si.cpuTemperature()
-    .then(result => socket.emit('response:status', result))
+  Promise.all([
+    si.cpuTemperature(),
+    si.mem()
+  ])
+    .then(([
+      cpuTemperature,
+      mem
+    ]) => {
+      socket.emit('response:status', {
+        cpuTemperature: `${cpuTemperature.main} ºC`,
+        memoryUsage: `${((mem.active / mem.total) * 100).toFixed(2)} %`
+      })
+    })
 }
 
 module.exports = status
