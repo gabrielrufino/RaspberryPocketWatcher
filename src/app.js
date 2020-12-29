310235d698be9c50b3ec3130908a77ecad6b7ed3
Gabriel Rufino <contato@gabrielrufino.com>
2020-12-29 11:30:00 -0300

    Passing query 'client' to the socket connection

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ module.exports = {
   start: () => {
     const { SERVER_URL } = process.env
 
-    const socket = io(SERVER_URL)
+    const socket = io(`${SERVER_URL}?client=watcher`)
 
     socket.on('connect', () => {
       console.log('Conectado')
